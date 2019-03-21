f53ab9f9a45ac035402fa3529fb0943d993be700
hacknife <hacknife@outlook.com>
2019-03-21 15:22:28 +0800

    demo

--- app/src/main/java/com/hacknife/sample/MainActivity.java
+++ app/src/main/java/com/hacknife/sample/MainActivity.java
@@ -2,9 +2,15 @@ package com.hacknife.sample;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.hacknife.refresh.core.RefreshLayout;
+import com.hacknife.refresh.core.api.Refresh;
+import com.hacknife.refresh.core.listener.OnRefreshListener;
 import com.hacknife.wifimanager.IWifi;
 import com.hacknife.wifimanager.IWifiManager;
 import com.hacknife.wifimanager.OnWifiChangeListener;
@@ -17,11 +23,25 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnWifiChangeListener, OnWifiConnectListener, OnWifiStateChangeListener {
     IWifiManager manager;
+    WifiAdapter adapter;
+    RecyclerView rc_view;
+    RefreshLayout refresh;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refresh = findViewById(R.id.refresh);
+        rc_view = findViewById(R.id.rc_view);
+        adapter = new WifiAdapter();
+        rc_view.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rc_view.setAdapter(adapter);
+        refresh.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull Refresh refreshLayout) {
+                manager.scanWifi();
+            }
+        });
         manager = WifiManager.create(this);
         manager.setOnWifiChangeListener(this);
         manager.setOnWifiConnectListener(this);
@@ -36,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements OnWifiChangeListe
 
     @Override
     public void onWifiChanged(List<IWifi> wifis) {
-
+        adapter.bindData(wifis);
     }
 
     @Override
